275f36a61ceff8c38eaf0a737d082c9d8d7aee85
TinoDCodes <tinodauti@gmail.com>
2025-07-07 23:36:05 +0200

    fix: date display in local time on meetings page

--- frontend/app/(betting)/meetings/page.tsx
+++ frontend/app/(betting)/meetings/page.tsx
@@ -9,13 +9,15 @@ export default async function MeetingsPage({
 }) {
   const { date } = await searchParams;
 
-  const selectedDate = date ? new Date(date) : new Date();
+  const selectedDate = moment(date).isValid()
+    ? moment(date).toDate()
+    : moment().toDate();
+
   const selectedDateFormatted =
     moment(selectedDate).format("dddd, MMMM D, YYYY");
 
   return (
     <div className="w-full flex flex-col items-center">
-      <p>{selectedDate.toDateString()}</p>
       <div className="w-fit py-1 px-3 border border-accent/15 rounded-xl text-xs text-accent mb-4">
         {selectedDateFormatted}
       </div>
